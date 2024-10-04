c58480da64ae56f74a0545ddbe3050a6b9d17c2f
Kai Franks <kafr1952@colorado.edu>
2024-10-03 23:14:08 -0600

    started chapter eight with a moveable sunset

---
+++ MyDevStoryKai/MyDevStoryKai/Font+MisterGrape.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  MyDevStoryKai
+//
+//  Created by Kai Franks on 10/3/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static func MisterGrape (size: CGFloat = 30) -> Font {
+        Font.custom("MisterGrape", size: size)
+    }
+}
