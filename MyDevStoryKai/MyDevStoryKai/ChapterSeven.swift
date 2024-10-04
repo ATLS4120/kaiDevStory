e445e74d5f31c11c79aa0e4c7f325b19ca015e12
Kai Franks <kafr1952@colorado.edu>
2024-10-03 21:34:24 -0600

    Server crashed and I edited the chapter seven

--- MyDevStoryKai/MyDevStoryKai/ChapterSeven.swift
+++ MyDevStoryKai/MyDevStoryKai/ChapterSeven.swift
@@ -11,49 +11,73 @@ import SwiftUI
 struct ChapterSeven: View {
     var body: some View {
         VStack {
-//            Text("Print ChapterVII")
-//                .font(.custom("AmericanTypewriter", size: 40).weight(.bold))
-//                .multilineTextAlignment(.center)
-//                .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
-//                .frame(width: 351, height: 0)
-//                .offset(y: 0)
-//                .underline()
-//                .padding(.bottom, 5)
-//            
-//            Text(Constants.ChapterSeven)
-//                .font(.custom("AmericanTypewriter", size: 20).weight(.bold))
-//                .multilineTextAlignment(.leading)
-//                .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
-//                .offset(y: 20)
-//                .padding(.bottom, 100)
-            
-            HStack {
-//                Image(systemName: "star")
-//                    .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
-//                Image(systemName: "star")
-//                    .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
-//                Image(systemName: "star")
-//                    .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+            VStack {
+                Color.blue
+                            Text("Print ChapterVII")
+                                .multilineTextAlignment(.center)
+                //                .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+                //                .frame(width: 351, height: 0)
+                //                .offset(y: 0)
+                //                .underline()
+                //                .padding(.bottom, 5)
+                HStack {
+                    Color.purple
+                    Color.green
+                    Color.red
+                }
+                ZStack {
+                    Color.yellow
+                    Text("Print ChapterVII")
+                        .multilineTextAlignment(.center)
+                }
             }
-            
-//            NavigationLink(destination: ChapterEight()) {
-//                Text("Next Chapter")
-//                    .frame(width: 150, height: 30)
-//                    .font(.custom("AmericanTypewriter", size: 20))
-//                    .padding()
-//                    .background(Color(red: 0.92, green: 0.93, blue: 0.84))
-//                    .foregroundColor(.black)
-//                    .cornerRadius(10)
-//            }
-            .padding([.top],50)
         }
-//        .navigationTitle("ChapterSeven")
-//        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
     }
-}
+
+//VStack {
+        //            Text("Print ChapterVII")
+        //                .font(.custom("AmericanTypewriter", size: 40).weight(.bold))
+        //                .multilineTextAlignment(.center)
+        //                .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+        //                .frame(width: 351, height: 0)
+        //                .offset(y: 0)
+        //                .underline()
+        //                .padding(.bottom, 5)
+        //
+        //            Text(Constants.ChapterSeven)
+        //                .font(.custom("AmericanTypewriter", size: 20).weight(.bold))
+        //                .multilineTextAlignment(.leading)
+        //                .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+        //                .offset(y: 20)
+        //                .padding(.bottom, 100)
+        
+        //            HStack {
+        //                Image(systemName: "star")
+        //                    .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+        //                Image(systemName: "star")
+        //                    .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+        //                Image(systemName: "star")
+        //                    .foregroundColor(Color(red: 0.92, green: 0.93, blue: 0.84))
+        //            }
+        
+        //            NavigationLink(destination: ChapterEight()) {
+        //                Text("Next Chapter")
+        //                    .frame(width: 150, height: 30)
+        //                    .font(.custom("AmericanTypewriter", size: 20))
+        //                    .padding()
+        //                    .background(Color(red: 0.92, green: 0.93, blue: 0.84))
+        //                    .foregroundColor(.black)
+        //                    .cornerRadius(10)
+        //            }
+        //            .padding([.top],50)
+        //        }
+        //       .navigationTitle("ChapterSeven")
+        //       .padding()
+        //        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //        .background(Color.black)
+    
+            }
+    
 #Preview {
         ChapterSeven()
 }
-
